91034b0db311605f56424d4febc7a473e96bb7ef
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-07-01 17:16:10 +0300

    Add method MyArray.prototype.push() for prototype of custom array MyArray

--- MyArray.js
+++ MyArray.js
@@ -12,3 +12,13 @@ function MyArray() {
 
 const myArrProto = new MyArray();
 MyArray.prototype = myArrProto;
+
+myArrProto.push = function push() {
+    for (let i = 0; i < arguments.length; i++) {
+        this[this.length++] = arguments[i];
+    }
+    return this.length;
+};
+
+
+
--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const customArray = new MyArray();
 console.log(`Output of "customArray.isMyArray()": ${customArray.isMyArray()}`);
 
-
+customArray.push(10, 7,8, 'a', 'b');
+console.log(customArray);
 
